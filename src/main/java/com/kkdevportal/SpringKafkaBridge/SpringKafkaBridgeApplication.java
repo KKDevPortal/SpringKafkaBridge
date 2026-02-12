e1e96b4d03be750ce271522d5a1ca3b73e203d9d
Kaushal Kishor <kaushal.kishor@niit.com>
2026-02-12 16:21:14 +0530

    readme and update

---
+++ src/main/java/com/kkdevportal/SpringKafkaBridge/SpringKafkaBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.kkdevportal.SpringKafkaBridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaBridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kkdevportal/SpringKafkaBridge/SpringKafkaBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkdevportal.SpringKafkaBridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
